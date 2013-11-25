db2935cbb71f6a65512d82c37a055c230e1d2544
baadnwz <baadnwz@gmail.com>
2013-11-25 12:49:59 +0000

    kitkat update inital commit

--- additions/htchacks/disablebytecode/byte.sh
+++
@@ -1,5 +0,0 @@
-#!/sbin/sh
-echo "" >> /system/build.prop
-echo "# Disable Bytecode Verification" >> /system/build.prop
-echo "dalvik.vm.verify-bytecode=false" >> /system/build.prop
-echo "dalvik.vm.dexopt-flags=v=n,o=v,m=y" >> /system/build.prop
