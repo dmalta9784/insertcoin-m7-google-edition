f41572964dc734259598286cbb2eb1e8bbd7d47c
baadnwz <baadnwz@gmail.com>
2013-11-26 14:10:08 +0000

    updated odex script

--- tools/odex.sh
+++ tools/odex.sh
@@ -11,26 +11,24 @@ export LOOP_MOUNTPOINT=/mnt/obb
 
 #BOOTCLASSPATH (read them from init.rc)
 export BOOTCLASSPATH="/system/framework/core.jar:/system/framework/conscrypt.jar:/system/framework/okhttp.jar:/system/framework/core-junit.jar:/system/framework/bouncycastle.jar:/system/framework/ext.jar:/system/framework/framework.jar:/system/framework/framework2.jar:/system/framework/telephony-common.jar:/system/framework/voip-common.jar:/system/framework/mms-common.jar:/system/framework/android.policy.jar:/system/framework/services.jar:/system/framework/apache-xml.jar:/system/framework/webviewchromium.jar:/system/framework/htcirlibs.jar"
-                      
+
 # Order matters here, up to BOOTCLASSPATH
 FRAMEWORK="/system/framework/core.jar
+/system/framework/conscrypt.jar
+/system/framework/okhttp.jar
 /system/framework/core-junit.jar
 /system/framework/bouncycastle.jar
 /system/framework/ext.jar
-/system/framework/HTCDev.jar
 /system/framework/framework.jar
 /system/framework/framework2.jar
+/system/framework/telephony-common.jar
+/system/framework/voip-common.jar
+/system/framework/mms-common.jar
 /system/framework/android.policy.jar
 /system/framework/services.jar
 /system/framework/apache-xml.jar
-/system/framework/com.htc.android.bluetooth.jar
-/system/framework/HTCCommonctrl.jar
-/system/framework/HTCExtension.jar
-/system/framework/com.orange.authentication.simcard.jar
-/system/framework/usbnet.jar
-/system/framework/telephony-common.jar
-/system/framework/voip-common.jar
-/system/framework/mms-common.jar"
+/system/framework/webviewchromium.jar
+/system/framework/htcirlibs.jar"
 
 # Odex Framework Rest
 REST="/system/framework/*.jar"
